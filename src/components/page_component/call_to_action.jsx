8bf173eea95de24fec93778f2f6f1f934381616b
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-09-27 18:17:39 -0700

    Add arrows to call to action

--- src/components/page_component/call_to_action.jsx
+++ src/components/page_component/call_to_action.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const CallToAction = ({ cta, party, style} ) => {
+const CallToAction = ({ cta, style} ) => {
 
   return(
         <div className="call-to-action" style={style}>
           <h2>{cta}</h2>
-          <button>LET'S TALK.</button>
+          <button>LET'S TALK. <span>&#187;</span></button>
         </div>
   );
 };
